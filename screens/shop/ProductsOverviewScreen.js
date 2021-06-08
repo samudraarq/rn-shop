31ba7775669e2264cefe0223c3457845172bd04a
Samudra Faria <samudrafaris@gmail.com>
2021-06-08 14:27:25 +0700

    init redux

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+const ProductsOverviewScreen = () => {
+  return <FlatList />;
+};
+
+export default ProductsOverviewScreen;
+
+const styles = StyleSheet.create({});
