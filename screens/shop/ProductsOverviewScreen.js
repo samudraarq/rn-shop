5d1927269e82b82272ce9031ab847fb271528728
Samudra Faria <samudrafaris@gmail.com>
2021-06-08 19:49:01 +0700

    product list display

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, FlatList, Text } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = () => {
   const products = useSelector((state) => state.products.availableProducts);
@@ -8,7 +9,15 @@ const ProductsOverviewScreen = () => {
   return (
     <FlatList
       data={products}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
     />
   );
 };
