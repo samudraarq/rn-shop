9629c4017bd179844fcd36456bfa716c6224eb20
Samudra Faris Arqam <samudrafaris@gmail.com>
2021-12-12 12:15:33 +0700

    add order item component

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import { useSelector } from "react-redux";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = () => {
   const orders = useSelector((state) => state.orders.orders);
   return (
     <FlatList
       data={orders}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+        />
+      )}
     />
   );
 };
