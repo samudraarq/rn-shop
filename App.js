0c3329d4c664697cef29cc11a59be106b06faada
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-01-04 19:57:23 +0700

    install redux-thunk

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
 // import { composeWithDevTools } from "redux-devtools-extension";
+import ReduxThunk from "react-redux";
 
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
@@ -16,7 +17,7 @@ const rootReducer = combineReducers({
   orders: ordersReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   const [loaded] = useFonts({
