9fb8a62d49c502476fc147aac70849ce2e73f72d
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-01-05 18:59:55 +0700

    save orders

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,10 +1,31 @@
-import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, FlatList, ActivityIndicator } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 import OrderItem from "../../components/shop/OrderItem";
+import Colors from "../../constants/Colors";
+
+import * as ordersActions from "../../store/actions/orders";
 
 const OrdersScreen = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const orders = useSelector((state) => state.orders.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+    dispatch(ordersActions.fetchOrders()).then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={orders}
@@ -21,4 +42,10 @@ const OrdersScreen = () => {
 
 export default OrdersScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
